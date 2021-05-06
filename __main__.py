27bc1406fa8af003541a87aba3f525c13d04df55
Vex87 <AndrewKyleGuilas@gmail.com>
2021-05-06 00:03:38 -0400

    announced texts with sound

--- __main__.py
+++ __main__.py
@@ -1,24 +1,67 @@
+TEMP_PATH = "__temp__"
+TTS_PATH = "__temp__/tts.mp3"
+
 import speech_recognition as sr
 import os
+from gtts import gTTS 
+import playsound
+import threading
+import queue
 
 r = sr.Recognizer()
+
+def create_queue(callback):
+    new_queue = queue.Queue()
+    threading.Thread(
+        target = callback,
+        args = (new_queue,),
+        daemon = True
+    ).start()
+
+    return new_queue
+
+def delete_contents(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def say_message(message: str):
+    audio = gTTS(text = message, lang = "en", tld = "com", slow = False) 
+    if not os.path.isdir(TEMP_PATH):
+        os.mkdir(TEMP_PATH)
+    delete_contents(TEMP_PATH)
+    audio.save(TTS_PATH) 
+    playsound.playsound(TTS_PATH)
 
 def copy_to_clip_board(text):
     command = "echo " + text.strip() + "| clip"
     os.system(command)
 
-while True:
-    with sr.Microphone() as source:
-        print("listening...")
-
-        text = None
-        while not text and text != "":
+def watch_voice(voice_queue):
+    while True:
+        with sr.Microphone() as source:
+            print("listening...")
             text = r.listen(source)
-
-        try:
             text = r.recognize_google(text)
+            if text and text != "":
+                voice_queue.put(text)
+
+def __main__():
+    global voice_queue
+    voice_queue = create_queue(watch_voice)
+
+    while True:
+        text = (voice_queue.qsize() > 0) and voice_queue.get()
+        if text:
             copy_to_clip_board(text)
             print(f"> {text}")
-        except Exception as error_message:
-            print(error_message)
+            say_message(text)
 
+if __name__ == "__main__":
+    __main__()
